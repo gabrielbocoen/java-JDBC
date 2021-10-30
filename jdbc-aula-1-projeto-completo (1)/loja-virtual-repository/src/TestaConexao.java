6373cbcdbeefed97d1d501c1df7aed4c556d69f1
Gabriel Boçoen <86474842+gabrielbocoen@users.noreply.github.com>
2021-10-30 14:56:00 -0300

    Add files via upload

---
+++ jdbc-aula-1-projeto-completo (1)/loja-virtual-repository/src/TestaConexao.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection con = DriverManager.
+				getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+
+		con.close();
+	}
+
+}
